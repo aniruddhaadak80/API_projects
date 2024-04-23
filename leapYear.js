2f35fda260311b5dc62a4fbe37c11d57c4096c9e
Aniruddha Adak <adakaniruddha9@gmail.com>
2024-04-23 22:09:36 +0530

    new file:   index.html         new file:   jokes.css         new file:   jokes.html         new file:   jokes.js         new file:   leapYear.js         new file:   quotes.js         new file:   style.css  and all api relatedprojects

---
+++ leapYear.js
@@ -0,0 +1,29 @@
+
+
+const inputYear = document.getElementById("inputYear");
+const submitBtn = document.getElementById("submitBtn");
+const result = document.getElementById("result");
+
+// Define the function to check for leap year
+let getYear = (year) => {
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    result.innerHTML = `${year} is a leap year .`;
+  } else {
+    result.innerHTML = `${year} is not a leap year .`;
+  }
+};
+
+// Add event listener to the button
+submitBtn.addEventListener("click", () => {
+  // Get the year from input field
+  const year = parseInt(inputYear.value);
+
+  // Check if input is a valid number
+  if (!isNaN(year)) {
+    // If it's a valid number, check if it's a leap year
+    getYear(year);
+  } else {
+    // If it's not a valid number, show an error
+    result.innerHTML = `${inputYear.value} is not a valid year!`;
+  }
+});
---
+++ quotes.js
@@ -0,0 +1,21 @@
+const generateBtn = document.getElementById("generateQuote");
+
+const displayQuotes = document.getElementById("displayQuotes");
+
+const authorname = document.getElementById("authorname");
+
+let getRandonQuote = async () => {
+  let api = await fetch("https://api.quotable.io/random");
+  let result = await api.json();
+
+  //   Display the quote
+  displayQuotes.innerHTML = result.content;
+
+  // Display the author name
+  authorname.innerHTML = `by ➡️ ${result.author}`;
+  console.table(result);
+};
+
+generateBtn.addEventListener("click", getRandonQuote);
+
+getRandonQuote();
